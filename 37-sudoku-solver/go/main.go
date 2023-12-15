a4586dd36d34e6bf33a3599b7c6da45fdac69c3f
idris <idris.guluzade@gmail.com>
2023-12-15 14:32:56 +0400

    37 complete go

--- 37-sudoku-solver/go/main.go
+++ 37-sudoku-solver/go/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"slices"
-)
-
-type Cell struct {
-	Row            int
-	Column         int
-	Box            int
-	PossibleValues []byte
-}
-
 func main() {
 	var board = [][]byte{
 		{'.', '.', '9', '7', '4', '8', '.', '.', '.'},
@@ -27,137 +14,47 @@ func main() {
 	}
 
 	solveSudoku(board)
-
 }
 
-func solveSudoku(board [][]byte) {
-	findNumber(board)
-	// for !isSolved(board) {
-	// }
+func isEligible(board [][]byte, row, col int, num byte) bool {
+	for x := 0; x < 9; x++ {
+		if board[row][x] == num {
+			return false
+		}
+		if board[x][col] == num {
+			return false
+		}
+		if board[3*(row/3)+x/3][3*(col/3)+x%3] == num {
+			return false
+		}
+	}
+	return true
 }
 
-// func isSolved(board [][]byte) bool {
-// 	for i := 0; i < len(board); i++ {
-// 		for j := 0; j < len(board[i]); j++ {
-// 			if board[i][j] == '.' {
-// 				return false
-// 			}
-// 		}
-// 	}
-// 	return true
-// }
-
-func findNumber(board [][]byte) {
-	var cells []Cell
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[i]); j++ {
+func solveSudoku(board [][]byte) bool {
+	row, col := -1, -1
+	isComplete := false
+outer_loop:
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
 			if board[i][j] == '.' {
-				existingValues := []byte{}
-
-				rowNumbers := getRowNumbers(board, i, existingValues)
-				columnNumbers := getColumnNumbers(board, j, existingValues)
-				boxNumbers, boxNum := getBoxNumbers(board, i, j, existingValues)
-
-				cells = append(cells, Cell{Row: i, Column: j, Box: boxNum, PossibleValues: possibleValues})
-
-				fmt.Println(possibleValues)
-				return
-
-				// if len(numbers) == 8 {
-				// 	var sum int
-				// 	for k, _ := range numbers {
-				// 		sum += int(k)
-				// 	}
-				// 	board[i][j] = byte(45 - sum + 48)
-				// 	fmt.Println()
-				// 	return
-				// }
+				row, col = i, j
+				isComplete = true
+				break outer_loop
 			}
 		}
 	}
-
-}
-
-func getRowNumbers(board [][]byte, rowIndex int, numbers []byte) []byte {
-	var values []byte
-	for _, b := range board[rowIndex] {
-		if b != '.' && !slices.Contains(numbers, b) {
-			values = append(values, b)
-		}
+	if !isComplete {
+		return true
 	}
-}
-
-func getBoxNumbers(board [][]byte, row int, column int, numbers []byte) ([]byte, int) {
-	var boxRow int
-	var boxColumn int
-	switch {
-	case row < 3:
-		boxRow = 1
-	case row < 6:
-		boxRow = 2
-	default:
-		boxRow = 3
-	}
-
-	switch {
-	case column < 3:
-		boxColumn = 0
-	case row < 6:
-		boxColumn = 1
-	default:
-		boxColumn = 2
-	}
-
-	boxNumber := boxColumn*3 + boxRow
-
-	boxY := int(math.Ceil(float64(row+1)/3)*3 - 3)
-	boxX := int(math.Ceil(float64(column+1)/3)*3 - 3)
-	var values []byte
-
-	for i := boxY; i <= boxY+2; i++ {
-		for j := boxX; j <= boxX+2; j++ {
-			curr := board[i][j]
-			if curr != '.' && !slices.Contains(numbers, curr) {
-				values = append(values, curr)
+	for num := byte('1'); num <= byte('9'); num++ {
+		if isEligible(board, row, col, num) {
+			board[row][col] = num
+			if solveSudoku(board) {
+				return true
 			}
+			board[row][col] = '.'
 		}
 	}
-	return values, boxNumber
+	return false
 }
-
-func getColumnNumbers(board [][]byte, columnIndex int, numbers []byte) []byte {
-	var values []byte
-	for _, row := range board {
-		val := row[columnIndex]
-		if val != '.' && !slices.Contains(numbers, b) {
-			values = append(values, val)
-		}
-	}
-}
-
-// func getColumns(board [][]byte) [][]byte {
-// 	var columns [][]byte
-// 	for j := 0; j < len(board[0]); j++ {
-// 		curr := []byte{}
-// 		for i := range board {
-// 			curr = append(curr, board[i][j])
-// 		}
-// 		columns = append(columns, curr)
-// 	}
-
-// 	return columns
-// }
-
-// func getBoxes(board [][]byte) [][]byte {
-// 	// boxY := int(math.Ceil(float64(row+1)/3)*3 - 3)
-// 	// boxX := int(math.Ceil(float64(column+1)/3)*3 - 3)
-
-// 	for i := 0; i <= 2; i++ {
-// 		for j := 0; j <= 2; j++ {
-// 			curr := board[i][j]
-// 			if curr != '.' {
-// 				numbers[curr-48] = true
-// 			}
-// 		}
-// 	}
-// }
