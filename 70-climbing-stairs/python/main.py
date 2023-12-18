7c1e80e1036f3fe448070b3c37358b3335043b62
idris <idris.guluzade@gmail.com>
2023-12-18 15:25:42 +0400

    70 - Python - solution improved

--- 70-climbing-stairs/python/main.py
+++ 70-climbing-stairs/python/main.py
@@ -1,16 +1,30 @@
 def main():
-    print(climbStairs(5))
+    solution = Solution()
+    print(solution.climbStairs(5))
+    print(solution.climbStairs(3))
+
+class Solution:
+    cache = {}
+
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        if n in self.cache.keys():
+            return self.cache[n]
+
+        self.cache[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.cache[n]
 
 
-def climbStairs(n):
-    prev = 1
-    prev2 = 0
 
-    for i in range(1, n+1):
-        curr = prev + prev2
-        prev2 = prev
-        prev = curr
-    return prev
+# Different implemnetioan
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        current, previous = 1, 0
 
+        for i in range(n):
+            previous, current = current, previous+current
+        return current
 
 main()
