beaa162702ff54825b7f415c031d90bab3f116ec
Idris <idris.quluzade@gmail.com>
2024-01-08 22:17:05 +0400

    112 - Go - solution added

---
+++ 112-path-sum/go/go.mod
@@ -0,0 +1,3 @@
+module 112-path-sum/go
+
+go 1.21.5
---
+++ 112-path-sum/go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	node := TreeNode{Val: 1, Left: nil, Right: nil}
+	fmt.Println(hasPathSum(&node, 1))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
